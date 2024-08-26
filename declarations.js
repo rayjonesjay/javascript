d60525c9ac5a9c8bec43b42fe01b99b614c646be
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-26 12:28:35 +0300

    declaration

--- declarations.js
+++ declarations.js
@@ -1,17 +1,21 @@
 const escapeStr = "`\\/\"'";
-console.log(escapeStr)
-const arr = Object.freeze([4, "2"]);
-const obj = Object.freeze({
+const arr = [4, "2"];
+const obj = {
   str: "I am the best",
   num: 67,
   bool: true,
   undef: undefined,
-});
+};
 const nested = {
-  arr: Object.freeze([4, undefined, "2"]),
-  obj: Object.freeze({
+  arr: [4, undefined, "2"],
+  obj: {
     str: "Father time",
     num: 33,
     bool: false,
-  }),
+  },
 };
+Object.freeze(arr);
+Object.freeze(obj);
+Object.freeze(nested);
+Object.freeze(nested,arr);
+Object.freeze(nested.obj);
