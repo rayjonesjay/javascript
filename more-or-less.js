df8d4c40c5e26635acc3e8724a7c1b5fbb77ae0e
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-26 22:36:56 +0300

    more or less ?

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+function more(arg1){
+    return arg1+=1;
+}
+function less(arg1) {
+    return arg1-=1;
+}
+function add(arg1,arg2) {
+    return arg1+arg2;
+}
+function sub(arg1,arg2) {
+    return arg1 - arg2;
+}
