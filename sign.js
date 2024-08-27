4168a59e209fd78d3d4ef3d541c9674ed543bd92
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-27 05:10:40 +0300

    sign

--- sign.js
+++ sign.js
@@ -1,8 +1,11 @@
-function sign(num){
-    if (num > 0){
+function sign(num) {
+    if (num > 0) {
         return 1
-    }else if (num < 0){
+    } else if (num < 0) {
         return -1
     }
     return 0
+}
+function sameSign(num1, num2){
+    return num1 > 0 && num2 > 0 || num1 < 0 && num2 < 0 || num1 === 0 && num2 === 0;
 }
