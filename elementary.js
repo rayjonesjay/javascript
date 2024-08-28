0cf33888f076140d36309cb1d08a639745ab68c6
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-29 02:41:32 +0300

    elementary

--- elementary.js
+++ elementary.js
@@ -1,10 +1,16 @@
 function divide(a,b){
   let res = 0
+  if (b===0){
+    throw new Error("Error Division by zero")
+  }
+  const isNeg = (a < 0 && b >0) || (a > 0&&b <0)
+  a = Math.abs(a)
+  b = Math.abs(b)
   while(a >= b){
     res++
     a = a - b
   }
-  return res
+  return isNeg ? -res: res
 }
 function multiply(a,b){
   let res = 0
@@ -22,14 +28,11 @@ function modulo(a,b){
   if (b===0){
     throw new Error("Error Division by zero")
   }
-  let isNeg = a < 0
+  let isNeg = a < 0;
   a = Math.abs(a)
   b = Math.abs(b)
   while(a>=b){
     a = a - b
   }
-  if (isNeg) {
-    a = -a
-  }
-  return a
+  return isNeg ? -a : a
 }
