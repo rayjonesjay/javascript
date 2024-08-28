e12aa421b87e19920354d0571429f5dbf57c661e
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-28 06:14:06 +0300

    collections

---
+++ collections.js
@@ -0,0 +1,29 @@
+function arrToSet(arr){
+    return new Set(arr);
+}
+function arrToStr(arr){
+    return arr.join('')
+}
+function setToArr(sett){
+    return Array.from(sett)
+}
+function setToStr(sett){
+  return sett.toString()
+}
+function strToArr(arr){
+  return arr.split()
+}
+function strToSet(str){
+
+}
+//  Object.fromEntries() converts an iterable
+function mapToObj(mp){
+  return Object.fromEntries(mp)
+}
+function
+const m = new Map();
+m.set('he','lo')
+console.log(mapToObj(m))
+console.log(setToArr([1,3,4,4]));
+console.log(arrToStr([1,2,3]))
+
