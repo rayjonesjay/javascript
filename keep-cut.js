4d87e31a62dd2b2efbf20571be14051670c726d6
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-29 09:27:24 +0300

    keep cut

---
+++ keep-cut.js
@@ -0,0 +1,53 @@
+function cutFirst(str){
+  let res = ""
+  for (let i = 2; i < str.length;i++){
+    res += str[i]
+  }
+  return res
+}
+function cutLast(str){
+  let res = ""
+  for(let i = (str.length-1)-2; i >= 0; i--){
+    res += str[i]
+  }
+  return res.split('').reverse().join('')
+}
+function cutFirstLast(str){
+  str = cutLast(str)
+  str = cutFirst(str)
+  return str
+}
+function keepFirst(str){
+  let res = ""
+  for(let i = 0; i < str.length;i++){
+    if (i < 2){
+      res += str[i]
+    }else {
+      break
+    }
+  }
+  return res
+}
+function keepLast(str){
+  let res = ""
+  let counter = 0
+  for(let i = str.length-2; i < str.length;i++){
+    if (counter < 2){
+      res += str[i]
+    }else {
+      break
+    }
+    counter += 1
+  }
+  return res
+}
+function keepFirstLast(s){
+ return keepFirst(s) + keepLast(s)
+}
+// let s = "12456"
+// console.log(cutFirst(s))
+// console.log(cutLast(s));
+// console.log(cutFirstLast(s))
+// console.log(keepFirst(s))
+// console.log(keepLast(s))
+// console.log(keepFirstLast(s))
