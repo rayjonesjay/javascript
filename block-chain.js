5eb5ca684e29e607cd6710911543893056139d29
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-29 06:31:59 +0300

    block chain

---
+++ block-chain.js
@@ -0,0 +1,20 @@
+// blockChain receives two arguments, data and prev which if not provided will use the genesis block
+function blockChain(data, prev = {index:0,hash:'0'}){
+  // new block gets new index
+  const index = prev.index+1;
+
+  // compute the hash using the provided function
+  const hash = hashcode(index + prev.hash + JSON.stringify(data));
+
+  // create the new block
+  const newBlock = {
+    index: index,
+    hash: hash,
+    data: data,
+    prev: prev,
+    chain: function(newData)  {
+      return blockChain(newBlock,this);
+    }
+  }
+  return newBlock
+}
