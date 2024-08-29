869f828cab8bb9c737beaa61c14c2d0bf0310248
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-29 08:59:11 +0300

    slicer

--- slicer.js
+++ slicer.js
@@ -1,4 +1,13 @@
 function slice(seq,start=0,optional=seq.length){
+  // need to detect if it is a string or array
+  let isArr = false
+  let isStr = false
+  if (Array.isArray(seq)){
+    isArr = true
+  }
+  if (typeof seq === 'string'){
+    isStr=true
+  }
   // seq means sequence
   if (optional<start){
     return []
@@ -22,7 +31,11 @@ function slice(seq,start=0,optional=seq.length){
   for(let i=start; i<optional;i++){
     res.push(seq[i])
   }
+  if (isStr){
+    return res.join('')
+  }
   return res
 }
-// let str = ["hello","world","golang","division","mangoes"]
-// console.log(slice(str,-23,43))
+let str = ["hello","world","golang","division","mangoes"]
+console.log(slice('abcdef', 2))
+console.log(slice(str,2))
