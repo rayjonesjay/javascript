541ab540c7c72f10382c956051509feb0b5c849b
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-29 08:06:13 +0300

    block chain

--- block-chain.js
+++ block-chain.js
@@ -1,3 +1,8 @@
+// const hashCode = str =>
+//   (
+//     [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
+//   ).toString(36)
+/*
 // blockChain receives two arguments, data and prev which if not provided will use the genesis block
 function blockChain(data, prev = {index:0,hash:'0'}){
   // new block gets new index
@@ -18,3 +23,43 @@ function blockChain(data, prev = {index:0,hash:'0'}){
   }
   return newBlock
 }
+*/
+function blockChain(data, prev = { index: 0, hash: '0' }) {
+  const block = {
+    index: prev.index + 1,
+    data: data,
+    prev: prev,
+    hash: hashCode(`${prev.index + 1}${prev.hash}${JSON.stringify(data)}`),
+    chain: function (newData) {
+      return blockChain(newData, this);
+    },
+  };
+
+  return block;
+}
+const first = blockChain({ a: 1 })
+console.log(first.index) //           -> 1
+console.log(first.data) //            -> { a: 1 }
+console.log(first.prev) //            -> { index: 0, hash: "0" }
+console.log(first.hash) //            -> '1103f27'
+console.log(hashCode('10{"a":1}')) // -> '1103f27'
+
+const second = first.chain({ hello: 'world' })
+console.log(second.hash) //                           -> '18drvvc'
+console.log(hashCode('21103f27{"hello":"world"}')) // -> '18drvvc'
+
+const chain = second
+  .chain({ value: 4455 })
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' })
+
+const fork = second
+  .chain({ value: 335 })
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' })
+
+console.log(chain.hash) //  -> '1qr3qfs'
+console.log(fork.hash) //   -> '1x9gsc1'
+console.log(chain.index) // -> 5
+console.log(fork.index) //  -> 5
+
