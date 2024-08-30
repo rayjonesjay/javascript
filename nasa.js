a1fbcdbdb520e8bcd58be1734869dfa54a6e3816
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-31 00:45:07 +0300

    nasa

--- nasa.js
+++ nasa.js
@@ -1,13 +1,21 @@
 function nasa(N){
   let res = ""
   for(let i = 1; i <= N; i++){
-    if(i !== N){
-      res += String(i) + " "
-    }else{
+    if(i % 3 === 0 && i % 5 ===0) {
+      res += "NASA"
+    }else if(i % 3 === 0){
+      res += "NA"
+    }else if(i % 5 === 0){
+      res += "SA"
+    }else {
       res += String(i)
     }
+    if(i !== N){
+      res += " "
+    }
   }
+  // console.log(res.split(''))
   return res
 }
 
-// console.log(nasa(10))
+// console.log(nasa(15))
