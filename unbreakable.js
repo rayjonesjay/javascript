d99cf159233995f76218bc82ba4db8f022f17444
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-30 23:57:12 +0300

    unbreak my javascript

--- unbreakable.js
+++ unbreakable.js
@@ -1,35 +1,42 @@
-function split(str){
+function split(str, sep) {
   let res = []
   let t = ""
-  for (let i = 0; i < str.length; i++){
-    if (str[i] !== ' '){
-      t += String(str[i])
-    }else {
-      if (t !== ""){
+  let i = 0
+  while (i < str.length) {
+    // check if the current substring matches the separator
+    if (str.slice(i, i + sep.length) === sep) {
+      if (t !== "") {
         res.push(t)
-        t = ""
+        t = "" // reset
       }
+      i = i + sep.length // move the i pointer to the end of sep
+    } else {
+      t += str[i];//accumulate
+      i++;
     }
+    // i++ //-> you can also accumulate from here
   }
-  if (t !== ""){
+  // take care of the last round
+  if (t !== "") {
     res.push(t)
     t = ""
   }
   return res
 }
 
-function join(arr){
+function join(arr, bridge) {
   let res = ""
-  for(let i = 0; i < arr.length;i++){
-    if(i < arr.length){
-      res += arr[i] + " "
-    }else {
-      res += arr[i]
+  for (let i = 0; i < arr.length; i++) {
+    const str = arr[i]
+    if (i !== 0) {
+      res += bridge
     }
+    res += str
   }
   return res
 }
-// let r = "hello world this is my time to shine"
-// let res = split(r)
-// console.log(res)
-// console.log(join(res))
+
+// // let r = "hello world this , is my time to shine"
+// let res = ["hello", "world", "this" , "is"]
+// // console.log(res)
+// console.log(join(res, "-"))
