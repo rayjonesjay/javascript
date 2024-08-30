6608c7ebc0ff8fb3f863d0d108aab8c3a07722a1
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-31 00:14:25 +0300

    unbreak my code

--- unbreakable.js
+++ unbreakable.js
@@ -5,22 +5,24 @@ function split(str, sep) {
   while (i < str.length) {
     // check if the current substring matches the separator
     if (str.slice(i, i + sep.length) === sep) {
-      // if (t !== "") {
+      if (t !== "") {
         res.push(t)
         t = "" // reset
-      // }
+      }
       i = i + sep.length // move the i pointer to the end of sep
     } else {
       t += str[i];//accumulate
-      // i++;
+      i++;
     }
-    i++
+
+
+    // i++
     // i++ //-> you can also accumulate from here
   }
   // take care of the last round
   // if (t !== "") {
-    res.push(t)
-    // t = ""
+  res.push(t)
+  // t = ""
   // }
   return res
 }
@@ -40,4 +42,4 @@ function join(arr, bridge) {
 // // let r = "hello world this , is my time to shine"
 // let res = ["hello", "world", "this" , "is"]
 // // console.log(res)
-// console.log(split('rrrr', 'rr'))
+// console.log(split('a b c', ' '))
