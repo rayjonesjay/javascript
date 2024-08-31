254bdc853064bfa6105c979d53f708ebb6394bc2
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-31 03:58:58 +0300

    letter space num

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,7 @@
 function letterSpaceNumber(str){
-  let pattern = /[a-zA-Z]\s\d(?![a-zA-Z]|[0-9])/;
+  let pattern = /[a-zA-Z]\s\d(?![a-zA-Z]|[0-9])/g;
   const matches = str.match(pattern)
   // console.log(matches);
-  return matches[0] || []
+  return matches || []
 }
-// console.log(letterSpaceNumber('example 1, example 20'))
+// console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
