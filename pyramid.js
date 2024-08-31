3eec21a8da968eb356fa005a903ce2f51051ffe0
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-31 03:14:45 +0300

    of giza

---
+++ pyramid.js
@@ -0,0 +1,19 @@
+function pyramid(str,num){
+  let res = ""
+
+  for(let i = 1; i <= num; i++){
+    let spaceCount = (num-i);
+    let spaces = " ".repeat(spaceCount);
+    // create the current row with the string repeated i times
+    let chars = str.repeat(2*i-1)
+    let row = spaces + chars + spaces;
+    // console.log(chars)
+    res += row;
+    if (i !== num){
+      res += "\n";
+    }
+  }
+  return res
+}
+let res = pyramid("*",5)
+console.log(res)
