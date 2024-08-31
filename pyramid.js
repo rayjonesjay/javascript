5f9f237853090c7be726074e297b067836f43d98
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-31 04:55:26 +0300

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -3,10 +3,10 @@ function pyramid(str,num){
 
   for(let i = 1; i <= num; i++){
     let spaceCount = (num-i);
-    let spaces = " ".repeat(spaceCount);
+    let spaces = " ".repeat(spaceCount).repeat(str.length);
     // create the current row with the string repeated i times
     let chars = str.repeat(2*i-1)
-    let row = spaces + chars;
+    let row = spaces + chars
     // console.log(chars)
     res += row;
     if (i !== num){
@@ -16,4 +16,5 @@ function pyramid(str,num){
   return res
 }
 let res = pyramid("*",5)
+res = console.log(pyramid('*', 5))
 console.log(res)
