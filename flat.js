2ee91bcd6b25d16315ee463d748219f1e44cb7ee
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-31 05:46:50 +0300

    flat

--- flat.js
+++ flat.js
@@ -1,1 +1,23 @@
-function flat(arr)
+function flat(arr, depth=1){
+  if (depth < 1) return arr;
+
+  let res = []
+
+  for(let i = 0; i < arr.length;i++){
+
+    const elem = arr[i]
+
+    if(Array.isArray(elem) && depth > 0){
+
+      res = res.concat(flat(elem,depth-1))
+
+    }else{
+
+      res.push(elem)
+
+    }
+
+  }
+
+  return res
+}
