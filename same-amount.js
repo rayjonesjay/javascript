8f4f023b354d12f3feaa2e2a2d5e3151aa8d4974
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-01 03:26:14 +0300

    same amount

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,16 @@
-function sameAmount(s,a,b){
-  return s.matchAll(a).length === s.matchAll(b).length
+function sameAmount(s, a, b) {
+  const regexA = new RegExp(a.source, 'g');
+  const regexB = new RegExp(b.source, 'g');
+
+  const matchesA = [...s.matchAll(regexA)].length;
+  const matchesB = [...s.matchAll(regexB)].length;
+
+  return matchesA === matchesB;
 }
+// console.log(sameAmount("abab", /a/, /b/)); // true
+// console.log(sameAmount("ababab", /a/, /b/)); // true
+// console.log(sameAmount("ababa", /a/, /b/)); // false
+// console.log(sameAmount("abxyz", /a/, /b/)); // true
 // const s = "hello world 123"
 // const a = /[a-z]\s[0-9]/g;
 // const b = /o\sw/g;
