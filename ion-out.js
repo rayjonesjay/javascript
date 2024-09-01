23682a1113654353a399b59b6598e5abde9805e2
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-01 03:51:56 +0300

    ionout

---
+++ ion-out.js
@@ -0,0 +1,18 @@
+function ionOut(s){
+ // split the input string into words
+  const words = s.split(/\s+/)
+
+  const res = [];
+
+  for(const word of words) {
+    const regex = /t(?=ion)/;
+    if (regex.test(word)){
+      res.push(word.replace('ion',''))
+    }
+  }
+  return res
+}
+// console.log(ionOut("action motion station nation attention")); // [ 'act', 'mot', 'stat', 'nat', 'attent' ]
+// console.log(ionOut("interaction friction tension")) // [ 'interact', 'frict', 'tens' ]
+// console.log(ionOut("destination nation ionization")) // [ 'destinat', 'nat' ]
+// console.log(ionOut("education caution pollution"))
