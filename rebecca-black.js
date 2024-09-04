acf28a4300a04689f1b2e1b64eb172b6d2179a02
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-04 09:32:05 +0300

    rebeccaa

--- rebecca-black.js
+++ rebecca-black.js
@@ -4,10 +4,17 @@ function isFriday(date){
 }
 function isWeekend(d){
     const date = new Date(d);
-    return d.getMonth() === 6 || d.getDate() === 0; // sun == 6 and sat == 6
+    return date.getDay() === 6 || date.getDay() === 0; // sun == 0 and sat == 6
 }
-function isLeapYear(d){
-    const date = new Date(d);
-    const year =  date.getFullYear()
-    return (year%4 === 0 && )
+// console.log(isWeekend('2024-09-01')) -> sunday->true
+function isLeapYear(date){
+    const d = new Date(date)
+    const yr = d.getFullYear();
+    return (yr % 4 == 0 || yr % 100) && yr % 400 !== 0
 }
+function isLastDayOfMonth(date){
+    const d = new Date(date)
+    const day = d.getDay()+1
+    return (day + 1) === 1
+}
+// console.log(isLastDayOfMonth('2024-09-01'))
