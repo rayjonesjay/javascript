06d1960fa83d0a08d0f18d55479432da009b7ef1
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-04 09:48:08 +0300

    reb

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,20 +1,26 @@
 function isFriday(date){
     const d = new Date(date);
-    return d.getFullYear() === 5; // friday is 5th sun is 0
+    return d.getDay() === 5; // friday is 5th sun is 0
 }
-function isWeekend(d){
-    const date = new Date(d);
-    return date.getDay() === 6 || date.getDay() === 0; // sun == 0 and sat == 6
+function isWeekend(date) {
+    const d = new Date(date);
+    const day = d.getDay();
+    return day === 6 || day === 0; // 0 represents Sunday and 6 represents Saturday
 }
+
 // console.log(isWeekend('2024-09-01')) -> sunday->true
-function isLeapYear(date){
-    const d = new Date(date)
-    const yr = d.getFullYear();
-    return (yr % 4 == 0 || yr % 100) && yr % 400 !== 0
+function isLeapYear(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
-function isLastDayOfMonth(date){
-    const d = new Date(date)
-    const day = d.getDay()+1
-    return (day + 1) === 1
+
+function isLastDayOfMonth(date) {
+    const d = new Date(date);
+    const testDate = new Date(d);
+    testDate.setDate(testDate.getDate() + 1);
+    return testDate.getDate() === 1; // If the next day is 1, then it's the last day of the month
 }
+
 // console.log(isLastDayOfMonth('2024-09-01'))
+console.log(isFriday('2014-08-29'))
