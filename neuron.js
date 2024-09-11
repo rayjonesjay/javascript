51a1ca544a402a972a12ed13b27fdd2116492b35
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-11 12:59:59 +0300

    neuron.js

---
+++ neuron.js
@@ -0,0 +1,23 @@
+/*
+Create a function named neuron that enables your AI/bot to learn to manipulate data into a more
+usable shape.
+
+neuron takes a slice of data to learn from and returns an object that is usable.
+*/
+
+function neuron(slice){
+    // step 1: evaluate the slice of strings using eval() function:
+}
+
+
+function eval(slice){
+
+}
+const res = neuron([
+    'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
+    'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
+    'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
+    'Orders: shutdown! - Response: Yes Sr!',
+    'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.'
+])
+console.log(res) // log the result
