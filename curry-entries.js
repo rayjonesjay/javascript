320b8936c7a109bc2a4a89e6513fc8aaed262c82
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-17 17:38:22 +0300

    js

---
+++ curry-entries.js
@@ -0,0 +1,9 @@
+/*
+Objective:
+Create a function: defaultCurry()() which curries two objects. 
+
+1.It merges the objects together.
+
+2.If the key exist in both objects, the value from the second object override the 
+value from the first object.
+*/
