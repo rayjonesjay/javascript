05ecad4d8351e1dd33471db1f771998436d72092
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-17 22:41:47 +0300

    tell me who

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -6,45 +6,35 @@ Number. LastName FirstName *starting from 1*
 
  */
 // Alice_John.json -> Alice John
-import { readdir } from 'node:fs/promises'
-import { resolve } from 'node:path'
-function splitName(str) {
-	let name = str.slice(0,-5).split('_')
-	return name 
-}
+import {readdir} from 'node:fs/promises'
+import {resolve} from 'node:path'
 
+function splitName(str) {
+	return str.slice(0, -5).split('_')
+}
 async function read(){
 	let dir = '.'
 	const args = process.argv.slice(2)
 
-	if(args.length != 0){
+	if(args.length !== 0){
 		 dir = args[0]
 	}
 	let path = resolve(dir)
-	let files = await readdir(path); // convert path to absolute
+	let files = await readdir(path);
 
-	// sort the names aphabetically
-	// files = files.sort(greate)
 	let newData = [];
 	for(let jsonName of files){
-		newData.push(splitName(jsonName))
+		let n = splitName(jsonName)
+		newData.push(n[1] + ' ' + n[0])
 	}
 
 	let count = 1;
-	newData = newData.sort()
+	newData.sort()
 	for(let name of newData) {
-		let formattedName = name.sort(greate).join(' ')
-		let format = `${count}. ${formattedName}`
+		let format = `${count}. ${name}`
 		console.log(format)
 		count++
 	}
 }
 
-function greate(a,b){
-	return a > b;
-}
-
-// console.log(read())
-// console.log(splitName("Alice_Ab.json").sort(greate))
-
-read().then()
+read().then().catch(err => console.log(err))
